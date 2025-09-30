23c47239c6bc5339b2ebece3ec7063d5f7d50884
DarkHydra151 <iliya.myakota@gmail.com>
2025-09-30 19:54:27 +0300

    Initial project setup with TypeScript and README

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let userName = "Iлля";
+let userAge = 20;
+let isStudent = true;
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Студент: ${isStudent}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let userName: string = "Iлля";
+let userAge: number = 20;
+let isStudent: boolean = true;
+
+console.log(`Ім'я: ${userName}`);
+console.log(`Вік: ${userAge}`);
+console.log(`Студент: ${isStudent}`);
